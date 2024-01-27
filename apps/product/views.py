89afa7af7cc40b7bf427d560bd732c3aa3c83a17
Khalil1T <tolonbekovkhalil@mail.ru>
2024-01-27 20:29:14 +0600

    product list

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,17 +1,17 @@
-from django.contrib.sites import requests
 from rest_framework import permissions, status
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-
 from .models import Product, Category
+from apps.user.models import User
+from apps.user.serializers import UserSerializer
 from .serializers import ProductSerializer, CategorySerializer
 
 
-class ProductListAPIView(generics.ListCreateAPIView):
-    serializer_class = ProductSerializer
+class ProductListAPIView(generics.ListAPIView):
     permission_classes = [permissions.AllowAny]
+    serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
 
@@ -20,14 +20,10 @@ class ProductCreateAPIView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
 
 
-
-# class ProductListAPIView(APIView):
-#     permission_classes = [permissions.AllowAny]
-#
-#     def get(self, request):
-#         objects = Product.objects.all()
-#         serializer = ProductSerializer(objects, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
+class CategoryListAPIView(generics.ListAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
 
 
 class CategoryCreateAPIView(APIView):
@@ -41,9 +37,3 @@ class CategoryCreateAPIView(APIView):
                 name = category_data['name']
                 Category.objects.get_or_create(name=name)
         return Response({'response': 'created'}, status=status.HTTP_201_CREATED)
-
-
-class CategoryListAPIView(generics.ListCreateAPIView):
-    serializer_class = CategorySerializer
-    permission_classes = [permissions.AllowAny]
-    queryset = Category.objects.all()
